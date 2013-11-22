68b4a85b4fe03d3876fc2e75b18fde4b43dd4d06
wibeasley <wibeasley@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-11-22 08:55:36 +0000

    Trying after moving around vignettes
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/nlsylinks@102 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/UtilityScripts/Refresh.R
+++ pkg/UtilityScripts/Refresh.R
@@ -13,16 +13,20 @@ devtools::run_examples(); dev.off() #This overwrites the NAMESPACE file too
 # devtools::run_examples(, "Ace.Rd")
 devtools::test()
 
-devtools::build()#args="--resave-data")
+devtools::build(args="--resave-data --no-build-vignettes")#args="--resave-data")
 # system("R CMD build --resave-data")
 
-#system("R CMD check D:/Projects/RDev/NlsyLinksStaging/NlsyLinks_1.205.tar.gz") #system("R CMD check D:/Projects/RDev/NlsyLinksStaging/NlsyLinks")
+
+system("R CMD build --resave-data --no-build-vignettes D:/Projects/RDev/NlsyLinksStaging/NlsyLinks")
+system("R CMD check D:/Projects/RDev/NlsyLinksStaging/NlsyLinks/NlsyLinks_1.300.tar.gz")
+#system("R CMD check D:/Projects/RDev/NlsyLinksStaging/NlsyLinks")
 # system("R CMD check --as-cran --no-clean D:/Projects/RDev/NlsyLinksStaging/NlsyLinks_1.205.tar.gz") #system("R CMD check D:/Projects/RDev/NlsyLinksStaging/NlsyLinks")
 devtools::check()#build_args="--resave-data")
+# devtools::check(build_args="--as-cran")
 
 devtools::revdep_check(pkg="NlsyLinks", recursive=TRUE)
 # devtools::revdep_check(pkg="Wats", recursive=TRUE)
-devtools::release(check=FALSE) #Careful, the last question ultimately uploads it to CRAN, where you can't delete/reverse your decision.
+# devtools::release(check=FALSE) #Careful, the last question ultimately uploads it to CRAN, where you can't delete/reverse your decision.
 
 mean(is.na(NlsyLinks::Links79PairExpanded[NlsyLinks::Links79PairExpanded$RelationshipPath=="Gen1Housemates", "RFull"]))
 sum(is.na(NlsyLinks::Links79PairExpanded[NlsyLinks::Links79PairExpanded$RelationshipPath=="Gen1Housemates", "RFull"]))
